d7b066803358b2c87d03f77c7d977a1d13ded06b
EvaBoxler <e.a.boksler@gmail.com>
2022-03-16 20:40:45 +0700

    vse

--- bin/src/games/even-logic.js
+++ bin/src/games/even-logic.js
@@ -1,5 +1,5 @@
-import engine from '../index.js';
-import getRandomNumber from '../getRandomNumber.js';
+import engine from "../index.js";
+import getRandomNumber from "../getRandomNumber.js";
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -8,7 +8,7 @@ const isEven = (number) => number % 2 === 0;
 const gameCheck = () => {
   const number = getRandomNumber(1, 100);
   const question = `${number}`;
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  const correctAnswer = isEven(number) ? "yes" : "no";
   return [question, correctAnswer];
 };
 
